f0102ea985efc88f381f68aae702b4836c229a36
nwabekeyi <chidi90simeon@gmail.com>
2024-05-13 14:08:09 +0100

    initial commit

---
+++ src/navBar.js
@@ -0,0 +1,20 @@
+const NavBar = () => {
+
+
+    return(
+        <section>
+        <nav>
+            <p>LOGO</p>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>FAQ</li>
+
+            </ul>
+        </nav>
+        </section>
+    )
+};
+
+export default NavBar;
