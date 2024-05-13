5664e4f28efb0fe0e211d5eef5e6636220069df6
nwabekeyi <chidi90simeon@gmail.com>
2024-05-13 15:15:56 +0100

    i removed another component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import NavBar from "./navBar";
-import AnotherNav from "./anotherNav";
 
 function App() {
   return (
     <div className="App">
      <NavBar />
-     <AnotherNav info="I am another navbar" />
     </div>
   );
 }
