b2581bcfe17e663778a1f4170e1a3fff6c9ec1a2
MelodyPrincee <felixprince8090@gmail.com>
2024-06-06 15:58:04 -0400

    route updated

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import HomePage from "./component/homePage/homePage";
 import ContactPage from "./component/contact/contactPage";
 import About from "./component/about/About";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Pricing from "./component/pricing/pricing";import SupportPage from "./component/supportNav/supportNav";
+import Pricing from "./component/pricing/pricing";
+import SupportPage from "./component/supportNav/supportNav";
 
 const MyRoute = () => {
   return (
