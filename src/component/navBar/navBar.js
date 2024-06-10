6fb28f4cb7440777fa8d1e991c22176d5540d365
Vanessageorgie <vanessageorgie0@gmail.com>
2024-06-10 14:56:18 -0700

    to fix tailwind issues

--- src/component/navBar/navBar.js
+++ src/component/navBar/navBar.js
@@ -38,7 +38,7 @@ const Navbar = () => {
         </div>
         <nav>
           <ul>
-            <li> <Link to="/" className='text-xl font-bold text-red-500'>HOME</Link></li>
+            <li> <Link to="/" className='font-bold text-red-500 '>HOME</Link></li>
             <li><Link to="/services">SERVICES</Link></li>
             <li><Link to="/pricing">PRICING</Link></li>
             <li><Link to="/contact">CONTACT</Link></li>
