c7b7cb55bee1f703bb958d5ba1ba5096379edf2e
nwabekeyi <chidi90simeon@gmail.com>
2024-06-12 12:46:45 +0100

    talwind bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
